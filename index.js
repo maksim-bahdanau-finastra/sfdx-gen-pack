6f40c756a8697bb0f00932ff19136e5a652e8dc1
Abhishek-finastra <63738504+Abhishek-finastra@users.noreply.github.com>
2021-05-07 15:51:32 +0100

    Reverting cmp with app exclusion

--- index.js
+++ index.js
@@ -184,15 +184,15 @@ program
                     var suffix = fileParts[1];
                     if (fileName.indexOf(".eslintrc.json") === -1)
                     {
-                       /* if (parts[1] === "aura" && suffix !== "cmp" && 
-                            suffix.indexOf("-meta") === -1)
+                        if (parts[1] === "aura" && suffix !== "cmp" && 
+                            suffix.indexOf("-meta") === -1 && parts[3].split(".")[1] !== "app")
                         { // Add the cmp 
                             var pathIndex = fileParts[0].lastIndexOf("/");
                             var cmpPath = fileParts[0].substr(0, pathIndex +1) + parts[2];
                             fileListForCopy.push(cmpPath + ".cmp"); 
                             fileListForCopy.push(cmpPath + ".cmp-meta.xml");
                         }
-                        else */ if (parts[1] === "lwc")
+                        else  if (parts[1] === "lwc")
                         { // Add the base components 
                             if (suffix !== "html")
                             {
